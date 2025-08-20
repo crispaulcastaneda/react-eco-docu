0e8f6c38c6ccbf3693234c297058bb434f019ca6
Cristian Paul Castañeda <cristianpaul_c@yahoo.com>
2025-08-20 14:27:43 +0800

    Configure using JSX, Writing nodes in list, Add json data, Create Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import pokemon from './pokemon.json';
+import PropTypes from 'prop-types';
+
+
+// Creating a component to display a list of Pokémon
+// This component receives a single Pokémon object as a prop and displays its name and type
+const PokemonList = ({ pokemon }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+  </tr>
+);
+
+// Defining prop types for the PokemonList component
+// This helps in validating the props passed to the component
+PokemonList.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: '0 auto',
+        width: 800,
+        paddingTop: '1rem',
+      }}
+    >
+      <h1 className="title">Pokemon Search</h1>
+      <table style={{ width: '100%' }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon.slice(0, 20).map(pokemon => (
+            <PokemonList key={pokemon.id} pokemon={pokemon} />
+          ))}
+
+        </tbody>
+      </table>
     </div>
   );
 }
